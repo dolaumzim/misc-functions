44d7786273822fc63ce52645ab66ad209fe05ea7
armando.assini <arm.assini@gmail.com>
2023-07-23 16:44:33 -0300

    fromPairs funcionando

--- src/fromPairs.ts
+++ src/fromPairs.ts
@@ -3,11 +3,16 @@ import { KeyAndValue } from '../types/keyAndValue';
 /**
  * Deve criar um objeto a partir da lista de arrays chave/valor
  *
- * Ex.: fromPairs(['a', 'a'], ['b': 1], ['c': false]) === { a: 'a', b: 1, c: false }
+ * Ex.: fromPairs(['a', 'a'], ['b', 1], ['c', false]) === { a: 'a', b: 1, c: false }
  *
  * @param args
  * @returns
  */
 export const fromPairs = (...args: KeyAndValue[]) => {
-  return {};
+  let result: object = {};
+
+  for (let i: number = 0; i < args.length; i++) {
+    result[args[i][0]] = args[i][1];
+  }
+  return result;
 };
