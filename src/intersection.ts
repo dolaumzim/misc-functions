a1e5c9fbc4512e21f9875350dccb23e4a202b371
armando.assini <arm.assini@gmail.com>
2023-07-23 18:31:26 -0300

    intersection funcionando

--- src/intersection.ts
+++ src/intersection.ts
@@ -8,5 +8,28 @@
  * @returns somente os itens definidos.
  */
 export const intersection = (left: number[], right: number[]): number[] => {
-  return null;
+
+  let result: number[] = [];
+  let counter: number = 0;
+
+  for (let i: number = 0; i < left.length; i++) {
+    for (let j: number = 0; j < right.length; j++) {
+      if (left[i] === right[j]) {
+        counter++;
+      }
+    }
+    if (counter != 0) {
+      counter = 0;
+      for (let k: number = 0; k < result.length; k++) {
+        if (left[i] === result[k]) {
+          counter++;
+        }
+      }
+      if (counter === 0) {
+        result.push(left[i]);
+      }
+    }
+    counter = 0;
+  }
+  return result;
 };
