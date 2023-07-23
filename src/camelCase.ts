1db0e91047d5fe36938625cf29492d85af49f389
armando.assini <arm.assini@gmail.com>
2023-07-23 11:10:26 -0300

    camelCase funcionando

--- src/camelCase.ts
+++ src/camelCase.ts
@@ -5,5 +5,11 @@
  * @returns o texto enviado por parâmetro, alterado para camelCase.
  */
 export const camelCase = (word: string): string => {
-  return '';
+  for (let i = 0; i < word.length; i++) {
+    if (word[i] === ' ' || word[i] === '_' || word[i] === '-') {
+      word = word.replace(word[i], '');
+      word = word.replace(word[i], word[i].toUpperCase());
+    }
+  }
+  return word;
 };
