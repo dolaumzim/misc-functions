7bd3665bb7722e5e2d81c407891a3e269db78a32
armando.assini <arm.assini@gmail.com>
2023-07-23 18:25:28 -0300

    group funcionando

--- src/group.ts
+++ src/group.ts
@@ -27,5 +27,15 @@ export const group = <T extends Record>(
   collection: T[],
   key: keyof T,
 ): { [key: string]: T[] } => {
-  return {};
+  let result = {};
+
+  for (let i: number = 0; i < collection.length; i++) {
+    if (!result[collection[i].country]) {
+      result[collection[i].country] = [];
+      result[collection[i].country].push(collection[i]);
+    } else {
+      result[collection[i].country].push(collection[i]);
+    }
+  }
+  return result;
 };
