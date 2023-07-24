3604f4c0fdaa115d0e0b41f4e7b9ba7bc9406f75
armando.assini <arm.assini@gmail.com>
2023-07-23 23:12:55 -0300

    kebabCase funcionando (todo lowercase)

--- src/kebabCase.ts
+++ src/kebabCase.ts
@@ -5,6 +5,7 @@
  * @returns o texto enviado por parâmetro, alterado para kebabCase.
  */
 export const kebabCase = (word: string): string => {
+  word = word.toLowerCase();
   for (let i = 0; i < word.length; i++) {
     if (word[i] === ' ' || word[i] === '_' || word[i] === '-') {
       word = word.replace(word[i], '-');
