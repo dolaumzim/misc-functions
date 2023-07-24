1564f936721c2a58d00b852ec9b15c9021d1c4d3
armando.assini <arm.assini@gmail.com>
2023-07-24 11:22:54 -0300

    uniq funcionando

--- src/uniq.ts
+++ src/uniq.ts
@@ -8,5 +8,28 @@
  * @returns somente os itens definidos.
  */
 export const uniq = <T>(args: T[]): T[] => {
-  return null;
+  let result: T[] = [];
+  let argsAux: any = args;
+  let counter: number = 0;
+  let pushed: number = 0;
+
+  for (let i: number = 0; i < args.length; i++) {
+    if (result.length === 0) {
+      result.push(argsAux[i]);
+      pushed++;
+    }
+    for (let j: number = 0; j < result.length; j++) {
+      if (!pushed || !counter) {
+        if (argsAux[i] === result[j]) {
+          counter++;
+        }
+      }
+    }
+    if (counter === 0 && pushed === 0) {
+      result.push(argsAux[i]);
+      pushed++;
+    }
+    counter = pushed = 0;
+  }
+  return result;
 };
