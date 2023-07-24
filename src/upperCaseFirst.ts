0754f2f148370da180ba4b5e75c0993d99fa97af
armando.assini <arm.assini@gmail.com>
2023-07-24 17:42:50 -0300

    upperCaseFirst funcionando

--- src/upperCaseFirst.ts
+++ src/upperCaseFirst.ts
@@ -1,1 +1,4 @@
-export const upperCaseFirst = (text: string): string => text;
+export const upperCaseFirst = (text: string): string => {
+    text = text.replace(text[0], text[0].toUpperCase());
+    return text;
+  };
