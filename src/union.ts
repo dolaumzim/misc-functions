e8474d3ae0fe5610b44fc62f7eba596b236c6909
armando.assini <arm.assini@gmail.com>
2023-07-24 11:08:14 -0300

    union funcionando

--- src/union.ts
+++ src/union.ts
@@ -9,5 +9,30 @@
  * @returns somente os itens definidos.
  */
 export const union = <T>(...args: T[]): T[] => {
-  return null;
+  let result: T[] = [];
+  let argsAux: any = args;
+  let counter: number = 0;
+  let pushed: number = 0;
+
+  for (let i: number = 0; i < args.length; i++) {
+    for (let j: number = 0; j < argsAux[i].length; j++) {
+      if (result.length === 0) {
+        result.push(argsAux[i][j]);
+        pushed++;
+      }
+      for (let k: number = 0; k < result.length; k++) {
+        if (!pushed || !counter) {
+          if (argsAux[i][j] === result[k]) {
+            counter++;
+          }
+        }
+      }
+      if (counter === 0 && pushed === 0) {
+        result.push(argsAux[i][j]);
+        pushed++;
+      }
+      counter = pushed = 0;
+    }
+  }
+  return result;
 };
