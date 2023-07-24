465833f74ad0ed563abdfc7b75822d2466305645
armando.assini <arm.assini@gmail.com>
2023-07-24 17:42:30 -0300

    uniqBy funcionando

--- src/uniqBy.ts
+++ src/uniqBy.ts
@@ -19,5 +19,28 @@ import { Record } from '../types/record';
  * @returns somente os itens definidos.
  */
 export const uniqBy = <T extends Record>(args: T[], key: keyof T): T[] => {
-  return null;
+  let result: T[] = [];
+  let argsAux: any = args;
+  let counter: number = 0;
+  let pushed: number = 0;
+
+  for (let i: number = 0; i < args.length; i++) {
+    if (result.length === 0 && argsAux[i].hasOwnProperty(key.toString())) {
+      result.push(argsAux[i]);
+      pushed++;
+    }
+    if (argsAux[i].hasOwnProperty(key.toString())) {
+      for (let j: number = 0; j < result.length; j++) {
+        if (result[j][key.toString()] === argsAux[i][key.toString()]) {
+          counter++;
+        }
+      }
+      if (counter === 0 && pushed === 0) {
+        result.push(argsAux[i]);
+        pushed++;
+      }
+    }
+    counter = pushed = 0;
+  }
+  return result;
 };
