bfcf3f195d43f3de566002ec9604a253fc86bd21
armando.assini <arm.assini@gmail.com>
2023-07-23 23:15:25 -0300

    startCase funcionando

--- src/startCase.ts
+++ src/startCase.ts
@@ -5,5 +5,13 @@
  * @returns o texto enviado por parâmetro, alterado para startCase.
  */
 export const startCase = (word: string): string => {
-  return '';
+  word = word.toLowerCase();
+  for (let i = 0; i < word.length; i++) {
+    if (word[i] === ' ' || word[i] === '_' || word[i] === '-') {
+      word = word.replace(word[i], ' ');
+      word = word.replace(word[i + 1], word[i + 1].toUpperCase());
+    }
+  }
+  word = word.replace(word[0], word[0].toUpperCase());
+  return word;
 };
